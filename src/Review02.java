38630ad35ed5339f1a9c245d274e710391dbe316
digital-shouen <shouen.hisamatsu@gmail.com>
2023-01-31 15:43:38 +0900

    Lesson4課題再提出

--- src/Review02.java
+++ src/Review02.java
@@ -5,14 +5,14 @@ public class Review02 {
       //1から100までの連続した整数を画面に表示
         for(int i = 1; i <= 100; i++ ) {
           
-          if(i % 3 == 0 )
-            System.out.println("Fizz"); //整数が3で割り切れる数なら "Fizz" と表示
+          if(i % 15 == 0)//整数が3で割り切れ、なおかつ5で割り切れる数なら "FizzBuzz" と表示
+            System.out.println("FizzBuzz"); 
      
-          else if(i % 5 == 0 )
-            System.out.println("Buzz"); //整数が5で割り切れる数なら "Buzz" と表示
+          else if(i % 3 == 0 )//整数が3で割り切れる数なら "Fizz" と表示
+            System.out.println("Fizz");
      
-          else if(i % 15 == 0 )
-            System.out.println("FizzBuzz"); //整数が3で割り切れ、なおかつ5で割り切れる数なら "FizzBuzz" と表示
+          else if(i % 5 == 0 ) //整数が5で割り切れる数なら "Buzz" と表示
+            System.out.println("Buzz"); 
      
           else
             System.out.println(i); //それ以外の数は、その数をそのまま表示
